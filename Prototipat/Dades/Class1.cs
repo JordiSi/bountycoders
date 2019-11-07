85d9fd5287cea98251c416d6c45f5333e7abb402
JordiSi <57010108+JordiSi@users.noreply.github.com>
2019-11-07 16:53:53 +0100

    Add files via upload

---
+++ Prototipat/Dades/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data.Common;
+
+namespace Dades
+{
+    public class Class1
+    {
+        private SqlDataAdapter adapter;
+        private SqlConnection conn;
+
+        public void Connectar()
+        {
+            string cnx = "Server = bountycoders.database.windows.net; Database = BountyCoders; User Id = bountycoders; Password = 123456aA;";
+            conn = new SqlConnection(cnx);
+        }
+
+        public DataSet PortarTaula(string query)
+        {
+            conn.Open();
+            adapter = new SqlDataAdapter(query, conn);
+
+            DataSet dts = new DataSet();
+            adapter.Fill(dts);
+
+            conn.Close();
+
+            return dts;
+        }
+
+        public void Actualitzar(DataSet dts)
+        {
+            conn.Open();
+            SqlCommandBuilder cmdBuilder;
+            cmdBuilder = new SqlCommandBuilder(adapter);
+            //DbCommand
+            if (dts.HasChanges()) {
+                adapter.Update(dts.Tables[0]);
+                //result = ?
+            }
+
+            conn.Close();
+        }
+        public void Executa(string query, DataSet dts)
+        {
+            //recoger manualmente (substring) los valores de los nombres de las columnas y los valores a añadir
+            //variable array recogida columnas y variable array recogida valores
+            conn.Open();
+
+            SqlCommand command = new SqlCommand(query, conn);
+            command.CommandType = CommandType.Text;
+            //command.Parameters.AddWithValue("@NOMBRECOLUMNA", VALOR);
+            int rows = command.ExecuteNonQuery();
+
+            conn.Close();
+        }
+    }
+}
---
+++ Prototipat/Prototipat/Taula.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Dades;
+
+namespace Prototipat
+{
+    public partial class Taula : Form
+    {
+        private string query;
+        private DataSet dts;
+        private Dades.Class1 dades;
+
+        public Taula()
+        {
+            InitializeComponent();
+            
+            dades = new Dades.Class1();
+            dades.Connectar();
+        }
+
+        private void btn_loadDTG_Click(object sender, EventArgs e)
+        {
+            query = "select * from planets";
+            dts = dades.PortarTaula(query);
+            dtgUsers.DataSource = dts.Tables[0];
+        }
+
+        private void btn_updateDTG_Click(object sender, EventArgs e)
+        {
+            dades.Actualitzar(dts);
+        }
+
+        private void btn_executeDTG_Click(object sender, EventArgs e)
+        {
+            if (Consulta.Text != "")
+            {
+                query = Consulta.Text;
+                dades.Executa(query, dts);
+            }
+            else
+            {
+                MessageBox.Show("Introdueix un valor per la consulta");
+            }
+        }
+
+        private void Taula_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
