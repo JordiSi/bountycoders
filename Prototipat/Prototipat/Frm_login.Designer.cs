6a1c2b587928304ba0c750405ba7b159ae3f66fa
Sergi Torrella <torryxd@gmail.com>
2020-01-22 16:00:13 +0100

    casicommit

--- Prototipat/Prototipat/Frm_login.Designer.cs
+++ Prototipat/Prototipat/Frm_login.Designer.cs
@@ -28,137 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_login));
-            this.label3 = new System.Windows.Forms.Label();
-            this.txt_password = new System.Windows.Forms.TextBox();
-            this.txt_user = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.login = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(210, 184);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(82, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Password:";
-            // 
-            // txt_password
-            // 
-            this.txt_password.Location = new System.Drawing.Point(309, 184);
-            this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(100, 20);
-            this.txt_password.TabIndex = 3;
-            this.txt_password.Click += new System.EventHandler(this.txt_password_Click);
-            this.txt_password.Leave += new System.EventHandler(this.txt_password_Leave);
-            // 
-            // txt_user
-            // 
-            this.txt_user.Location = new System.Drawing.Point(309, 129);
-            this.txt_user.Name = "txt_user";
-            this.txt_user.Size = new System.Drawing.Size(100, 20);
-            this.txt_user.TabIndex = 2;
-            this.txt_user.Click += new System.EventHandler(this.txt_user_Click);
-            this.txt_user.Leave += new System.EventHandler(this.txt_user_Leave);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(245, 129);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "User:";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Gold;
-            this.label1.Location = new System.Drawing.Point(32, 36);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(465, 61);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Login Secure Core";
-            // 
-            // login
-            // 
-            this.login.BackColor = System.Drawing.Color.Gold;
-            this.login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.login.Location = new System.Drawing.Point(427, 152);
-            this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(100, 32);
-            this.login.TabIndex = 1;
-            this.login.Text = "LOG IN";
-            this.login.UseVisualStyleBackColor = false;
-            this.login.Click += new System.EventHandler(this.login_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(408, 281);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "No pulses este boton";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Prototipat.Properties.Resources.c953b2e1a607b064b71ca1ce6e2dba0f;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 114);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(192, 143);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // axWindowsMediaPlayer1
-            // 
-            this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(43, 152);
-            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
-            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(111, 61);
-            this.axWindowsMediaPlayer1.TabIndex = 6;
-            // 
-            // Form_login
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = global::Prototipat.Properties.Resources.stars;
-            this.ClientSize = new System.Drawing.Size(539, 304);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.login);
-            this.Controls.Add(this.txt_user);
-            this.Controls.Add(this.txt_password);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.axWindowsMediaPlayer1);
-            this.Cursor = System.Windows.Forms.Cursors.Default;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Name = "Form_login";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Login";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_login));
+			this.label3 = new System.Windows.Forms.Label();
+			this.txt_password = new System.Windows.Forms.TextBox();
+			this.txt_user = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.login = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label3.Location = new System.Drawing.Point(210, 184);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(82, 20);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Password:";
+			// 
+			// txt_password
+			// 
+			this.txt_password.Location = new System.Drawing.Point(309, 184);
+			this.txt_password.Name = "txt_password";
+			this.txt_password.Size = new System.Drawing.Size(100, 20);
+			this.txt_password.TabIndex = 3;
+			this.txt_password.Click += new System.EventHandler(this.txt_password_Click);
+			this.txt_password.Leave += new System.EventHandler(this.txt_password_Leave);
+			// 
+			// txt_user
+			// 
+			this.txt_user.Location = new System.Drawing.Point(309, 129);
+			this.txt_user.Name = "txt_user";
+			this.txt_user.Size = new System.Drawing.Size(100, 20);
+			this.txt_user.TabIndex = 2;
+			this.txt_user.Click += new System.EventHandler(this.txt_user_Click);
+			this.txt_user.Leave += new System.EventHandler(this.txt_user_Leave);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+			this.label2.Location = new System.Drawing.Point(245, 129);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(47, 20);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "User:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.Gold;
+			this.label1.Location = new System.Drawing.Point(32, 36);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(465, 61);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Login Secure Core";
+			// 
+			// login
+			// 
+			this.login.BackColor = System.Drawing.Color.Gold;
+			this.login.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.login.Location = new System.Drawing.Point(427, 152);
+			this.login.Name = "login";
+			this.login.Size = new System.Drawing.Size(100, 32);
+			this.login.TabIndex = 1;
+			this.login.Text = "LOG IN";
+			this.login.UseVisualStyleBackColor = false;
+			this.login.Click += new System.EventHandler(this.login_Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(408, 281);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(131, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "No pulses este boton";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Prototipat.Properties.Resources.c953b2e1a607b064b71ca1ce6e2dba0f;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 114);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(192, 143);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// axWindowsMediaPlayer1
+			// 
+			this.axWindowsMediaPlayer1.Enabled = true;
+			this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(43, 152);
+			this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+			this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+			this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(111, 61);
+			this.axWindowsMediaPlayer1.TabIndex = 6;
+			// 
+			// Form_login
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.BackgroundImage = global::Prototipat.Properties.Resources.stars;
+			this.ClientSize = new System.Drawing.Size(539, 304);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.login);
+			this.Controls.Add(this.txt_user);
+			this.Controls.Add(this.txt_password);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.axWindowsMediaPlayer1);
+			this.Cursor = System.Windows.Forms.Cursors.Default;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form_login";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Login";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
