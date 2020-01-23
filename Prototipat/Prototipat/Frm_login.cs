3721eae7e6e8baa619782e75fa6fc20ee5afa015
josepete19 <jr.rojas9619@gmail.com>
2020-01-23 20:05:01 +0100

    Necesita arreglo torry

--- Prototipat/Prototipat/Frm_login.cs
+++ Prototipat/Prototipat/Frm_login.cs
@@ -13,19 +13,18 @@ using Dades;
 
 namespace Prototipat
 {
-    public partial class Form_login: Form
+    public partial class Form_login : Form
     {
         public DataSet dts;
         private Class1 dades;
         private SqlConnection conn;
-		public Form_splash frmSplash;
-		public Form_principal frmMenu;
+        public Form_splash frmSplash;
+        public Form_principal frmMenu;
 
         public Form_login()
         {
             InitializeComponent();
         }
-        //dts = dades.PortarTaula(query);
         private void Form1_Load(object sender, EventArgs e)
         {
             dades = new Dades.Class1();
@@ -46,7 +45,8 @@ namespace Prototipat
 
         private void txt_user_Enter(object sender, EventArgs e)
         {
-            if (txt_user.ForeColor == Color.Gray) {
+            if (txt_user.ForeColor == Color.Gray)
+            {
                 txt_user.ForeColor = Color.Black;
                 txt_user.Text = "";
             }
@@ -54,7 +54,8 @@ namespace Prototipat
 
         private void txt_user_Leave(object sender, EventArgs e)
         {
-            if (txt_user.Text == "") {
+            if (txt_user.Text == "")
+            {
                 txt_user.ForeColor = Color.Gray;
                 txt_user.Text = "User";
             }
@@ -62,7 +63,8 @@ namespace Prototipat
 
         private void txt_password_Enter(object sender, EventArgs e)
         {
-            if (txt_password.ForeColor == Color.Gray) {
+            if (txt_password.ForeColor == Color.Gray)
+            {
                 txt_password.ForeColor = Color.Black;
                 txt_password.Text = "";
                 txt_password.PasswordChar = '*';
@@ -71,54 +73,93 @@ namespace Prototipat
 
         private void txt_password_Leave(object sender, EventArgs e)
         {
-            if (txt_password.Text == "") {
+            if (txt_password.Text == "")
+            {
                 txt_password.ForeColor = Color.Gray;
                 txt_password.Text = "Password";
                 txt_password.PasswordChar = char.Parse("\0");
             }
         }
 
-		private void successfulLogin(){ //Si la contrasenya és correcte
-			Form_splash frm;
-			if(frmSplash != null){
-				frm = frmSplash;
-			}else{
-				frm = new Form_splash();
-			}
-			frm.Show();
-			frm.frmLogin = this;
-			if(frmMenu != null)
-				frm.frmMenu = frmMenu;
-			this.Hide();
-		}
+        //<<<<<<< HEAD
+        private void successfulLogin()
+        { 
+            Form_splash frm;
+            if (frmSplash != null)
+            {
+                frm = frmSplash;
+            }
+            else
+            {
+                frm = new Form_splash();
+            }
+            frm.Show();
+            frm.frmLogin = this;
+            if (frmMenu != null)
+                frm.frmMenu = frmMenu;
+            this.Hide();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form_splash frm;
+            if (frmSplash != null)
+            {
+                frm = frmSplash;
+            }
+            else
+            {
+                frm = new Form_splash();
+            }
+            frm.accesslevel = 100;
+            frm.Show();
+            frm.frmLogin = this;
+            if (frmMenu != null)
+                frm.frmMenu = frmMenu;
+            this.Hide();
+        }
 
         private void login_Click(object sender, EventArgs e)
         {
-            string query = "select * from Users "; 
-            //SqlDataReader executereader
-            if (txt_user.Text!= "" && txt_password.Text != "")
+            int accesslevel;
+            string query = "select AccessLevel from Users, usercategories ";
+            if (txt_user.Text != "" && txt_password.Text != "")
             {
                 conn = dades.Connexio();
                 conn.Open();
-                query += "where UserName=" + "'" +txt_user.Text + "'" + " and Password=" + "'" + txt_password.Text + "'";
+                query += "where users.idUserCategory=usercategories.idUserCategory and UserName=" + "'" + txt_user.Text + "'" + " and Password=" + "'" + txt_password.Text + "'";
                 SqlCommand cmdBuilder = new SqlCommand(query, conn);
                 SqlDataReader reader = cmdBuilder.ExecuteReader();
+                
 
-                if (reader.Read()) {
-					successfulLogin();
-                } else {
+                if (reader.Read())
+                {
+                    accesslevel = reader.GetInt32(0);
+                    Form_splash frm;
+                    if (frmSplash != null)
+                    {
+                        frm = frmSplash;
+                    }
+                    else
+                    {
+                        frm = new Form_splash();
+                    }
+                    conn.Close();
+                    frm.accesslevel = accesslevel;
+                    frm.Show();
+                    frm.frmLogin = this;
+                    if (frmMenu != null)
+                        frm.frmMenu = frmMenu;
+                    this.Hide();
+                }
+                else
+                {
                     MessageBox.Show("Login incorrecte");
                 }
-				conn.Close();
             }
             else
             {
                 MessageBox.Show("Introdueix les dades");
             }
         }
-
-		private void button1_Click(object sender,EventArgs e) {
-			successfulLogin();
-		}
-	}
+    }
 }
--- Prototipat/Prototipat/MenuOptions.cs
+++ Prototipat/Prototipat/MenuOptions.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,111 +17,149 @@ using Species;
 using UserCategories;
 using UserRanks;
 using Users;
+using Dades;
+using System.Data.SqlClient;
 
 namespace Prototipat
 {
-	public partial class Form_principal : Form
-	{
-		public Form_login frmLogin;
-		public Form_splash frmSplash;
+    public partial class Form_principal : Form
+    {
+        public Form_login frmLogin;
+        public Form_splash frmSplash;
 
-		public Form_principal()
-		{
-			InitializeComponent();
-		}
+        public int accesslevel_usuari, accesslevel_taula;
 
-		private void planetesToolStripMenuItem_Click(object sender,EventArgs e) {
-			frm_Planetes frm = new frm_Planetes();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
+        private Class1 dades;
+        private string query;
+        public Form_principal()
+        {
+            InitializeComponent();
         }
-		private void filiationsToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_Filiations frm = new frm_Filiations();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void regionsToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_Regions frm = new frm_Regions();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void routesToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_Routes frm = new frm_Routes();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void routeTypesToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_RouteTypes frm = new frm_RouteTypes();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void sectorsToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_Sectors frm = new frm_Sectors();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void speciesToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_Species frm = new frm_Species();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void userCategoriesToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_UserCategories frm = new frm_UserCategories();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void userRanksToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_UserRanks frm = new frm_UserRanks();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
-		private void usersToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			frm_Users frm = new frm_Users();
-			frm.MdiParent = this;
-			frm.Show();
-			ocultar();
-		}
+        private void Form_principal_Load(object sender, EventArgs e)
+        {
+            dades = new Dades.Class1();
 
-		private void frmPrincipal_FormClosing(object sender,FormClosingEventArgs e) {
-			salir();
-		}
+            foreach (Control ctr1 in this.Controls)
+            {
+                if (ctr1.Name.Equals("menuStrip1"))
+                {
+                    foreach (ToolStripMenuItem ctr2 in ((MenuStrip)ctr1).Items)
+                    {
+                        if (ctr2.Text != "Logout" && ctr2.Text != "Exit")
+                        {
+                            query = "select accesslevel from menuaccess where table_name=" + "'" + ctr2.Text + "'";
+                            var dataset = dades.PortarTaula(query);
+                            accesslevel_taula = (int)dataset.Tables[0].Rows[0][0];
+                            if (accesslevel_taula > accesslevel_usuari)
+                            {
+                                ctr2.Visible = false;
+                            }
+                        }
+                    }
+                }
+            }
 
-		private void sortirToolStripMenuItem_Click(object sender,EventArgs e) {
-			salir();
-		}
+        }
+        private void planetesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Planetes frm = new frm_Planetes();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void filiationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Filiations frm = new frm_Filiations();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void regionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Regions frm = new frm_Regions();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void routesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Routes frm = new frm_Routes();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void routeTypesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_RouteTypes frm = new frm_RouteTypes();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void sectorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Sectors frm = new frm_Sectors();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void speciesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Species frm = new frm_Species();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void userCategoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_UserCategories frm = new frm_UserCategories();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void userRanksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_UserRanks frm = new frm_UserRanks();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
+        private void usersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_Users frm = new frm_Users();
+            frm.MdiParent = this;
+            frm.Show();
+            ocultar();
+        }
 
-		private void loginToolStripMenuItem_Click(object sender,EventArgs e) {
-			Form_login frm = frmLogin;
-			frm.Show();
-			frm.frmMenu = this;
-			frm.frmSplash = frmSplash;
-			this.Hide();
+        private void frmPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            salir();
+        }
 
-		}
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            salir();
+        }
 
-		public void ocultar() {
-			pictureBox1.Visible = false;
-		}
-		public void salir(){
-			Application.Exit();
-		}
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form_login frm = frmLogin;
+            frm.Show();
+            frm.frmMenu = this;
+            frm.frmSplash = frmSplash;
+            this.Hide();
+
+        }
+
+        public void ocultar()
+        {
+            pictureBox1.Visible = false;
+        }
+        public void salir()
+        {
+            Application.Exit();
+        }
+
+
     }
 }
